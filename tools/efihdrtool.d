0686a56b4818a35f8b2f78253b563e0f2a1c6053
Jakub Szewczyk <kubasz51@gmail.com>
2018-04-26 12:37:48 +0200

    Work on code generation

--- tools/efihdrtool.d
+++ tools/efihdrtool.d
@@ -9,8 +9,8 @@
 /// It outputs files to source/pack/age/module.d unless otherwise specified
 module efihdrtool;
 
-import std.stdio, std.string, std.array, std.algorithm, std.range,
-    std.format, std.conv;
+import std.stdio, std.string, std.array, std.algorithm, std.range, std.format,
+    std.conv;
 import std.path, std.process, std.getopt;
 import std.file : isFile, exists, mkdirRecurse, readText;
 import pegged.grammar;
@@ -24,7 +24,7 @@ struct GeneratedModule
     this(string name)
     {
         this.name = name;
-        this.dpath = name.translate(['.':'/']) ~ ".d";
+        this.dpath = name.translate(['.' : '/']) ~ ".d";
     }
 
     string name;
@@ -47,7 +47,7 @@ int main(string[] args)
         "o|outputPath", "The source root for the outputted modules", &outPathRoot
     );
     // dfmt on
-    if(args.length != 2 || gor.helpWanted)
+    if (args.length != 2 || gor.helpWanted)
     {
         defaultGetoptPrinter("dub run --single efihdrtool.d -- [options] specfile.map", gor.options);
         return 1;
@@ -57,7 +57,7 @@ int main(string[] args)
     {
         loadMapping(File(args[1], "r"));
     }
-    catch(Exception e)
+    catch (Exception e)
     {
         writeln(e.msg);
         return 1;
@@ -65,28 +65,28 @@ int main(string[] args)
 
     // look for nonexistant files
     string[] nonexistant;
-    foreach(hpath, mod; headerToModuleMappings)
+    foreach (hpath, mod; headerToModuleMappings)
     {
         string hfullpath = buildNormalizedPath(inPathRoot, hpath);
-        if(!exists(hfullpath) || !isFile(hfullpath))
+        if (!exists(hfullpath) || !isFile(hfullpath))
             nonexistant ~= hfullpath;
         string dfulldir = buildNormalizedPath(outPathRoot, mod.dpath).dirName;
-        if(!exists(dfulldir))
+        if (!exists(dfulldir))
             mkdirRecurse(dfulldir);
     }
-    if(nonexistant.length > 0)
+    if (nonexistant.length > 0)
     {
         writefln("The following files could not be found: \n%(%s\n%)", nonexistant);
         return 1;
     }
 
-    foreach(dmod; modules)
+    foreach (dmod; modules)
     {
-        foreach(hpath; dmod.hpaths)
+        foreach (hpath; dmod.hpaths)
         {
             string hfullpath = buildNormalizedPath(inPathRoot, hpath);
             string htext = readText(hfullpath);
-            processCode(dmod, htext);
+            processCode(dmod, htext, hpath);
         }
     }
 
@@ -95,57 +95,223 @@ int main(string[] args)
 
 void loadMapping(File mapFile)
 {
-    foreach(lineR; mapFile.byLine)
+    foreach (lineR; mapFile.byLine)
     {
         auto line = lineR.strip;
-        if(line.length < 1)
+        if (line.length < 1)
             continue;
-        if(line[0] == '#')
+        if (line[0] == '#')
             continue;
-        if(auto spl = line.findSplit("="))
+        if (auto spl = line.findSplit("="))
         {
             string hpath = spl[0].strip.idup;
             string dmod = spl[2].strip.idup;
-            if(dmod !in modules)
+            if (dmod !in modules)
                 modules[dmod] = new GeneratedModule(dmod);
             modules[dmod].hpaths ~= hpath;
             headerToModuleMappings[hpath] = modules[dmod];
         }
         else
         {
-            throw new Exception("Malformed input line: "~lineR.idup);
+            throw new Exception("Malformed input line: " ~ lineR.idup);
         }
     }
 }
 
-void processCode(GeneratedModule* dmod, string htext)
+void processCode(GeneratedModule* dmod, string htext, string hpath)
 {
     import std.regex;
+
     htext = htext.replaceAll(regex(`\\\r?\n`, "gs"), "");
     ParseTree p = CCode(htext);
     import pegged.tohtml;
+
     toHTML(p, File("parseresult.html", "w"));
+    dmod.code ~= "\n//=====================================\n// ";
+    dmod.code ~= hpath;
+    dmod.code ~= "\n//=====================================\n";
+
+    void parse(ParseTree pt)
+    {
+        switch (pt.name)
+        {
+        case "CCode":
+            return parse(pt.children[0]);
+        case "CCode.Unit":
+            {
+                foreach (i, ref c; pt.children)
+                {
+                    switch (c.name)
+                    {
+                    case "CCode.PIf":
+                        if (c.matches[1].endsWith("_H__")) // ignore header guards
+                            break;
+                        // TODO:Handle #if
+                        break;
+                    case "CCode.PElif":
+                        // TODO: Handle #elif
+                        break;
+                    case "CCode.PEndIf":
+                        // TODO:Handle #endif
+                        break;
+                    default:
+                        parse(c);
+                    }
+                }
+                break;
+            }
+        case "CCode.DocComment":
+            foreach (m; pt.matches)
+                dmod.code ~= m;
+            dmod.code ~= '\n';
+            break;
+        case "CCode.PError":
+            dmod.code ~= "static assert(0, `";
+            dmod.code ~= pt.matches[1];
+            dmod.code ~= "`);\n";
+            break;
+        case "CCode.Include":
+            string ipath = pt.children[0].matches[0];
+            GeneratedModule** mpp = ipath in headerToModuleMappings;
+            if (mpp !is null)
+            {
+                if ((*mpp) is dmod)
+                    break;
+                dmod.code ~= "import ";
+                dmod.code ~= (*mpp).name;
+                dmod.code ~= ";\n";
+            }
+            else
+            {
+                dmod.code ~= "//HDRERROR: Missing mapping to " ~ ipath ~ "\n";
+                writeln("Missing mapping to ", ipath);
+            }
+            break;
+        case "CCode.DefineConstant":
+            {
+                string id = pt.children[0].matches[0];
+                if (id.endsWith("_H__")) // ignore header guards
+                    break;
+                string value = pt.children.length == 1 ? "true" : pt.children[1].matches[0];
+                dmod.code ~= "enum ";
+                dmod.code ~= id;
+                dmod.code ~= " = ";
+                dmod.code ~= value;
+                dmod.code ~= ";\n";
+                break;
+            }
+        case "CCode.AliasTypedef":
+            {
+                if (pt.children[0].name == "CCode.FuncPointerVar")
+                {
+                    // TODO
+                    break;
+                }
+                // else: if(pt.children[0].name == "CCode.Type")
+                ParseTree tpt = pt.children[0].children[0];
+                string id = pt.children[1].matches[0];
+                if (tpt.name == "CCode.StructType" || tpt.name == "CCode.UnionType")
+                {
+                    // TODO
+                    break;
+                }
+                int stars = 0;
+                while (tpt.name == "CCode.Pointer")
+                {
+                    tpt = tpt.children[0].children[0];
+                    stars++;
+                }
+                assert(tpt.name == "CCode.Identifier");
+                dmod.code ~= "alias ";
+                dmod.code ~= id;
+                dmod.code ~= " = ";
+                dmod.code ~= tpt.matches[0];
+                foreach (i; 0 .. stars)
+                    dmod.code ~= '*';
+                ParseTree* arrPostfix = (&pt).getChild("CCode.ArrayPostfix");
+                if (arrPostfix !is null)
+                    dmod.code ~= arrPostfix.input[arrPostfix.begin .. arrPostfix.end];
+                dmod.code ~= ";\n";
+                break;
+            }
+        case "CCode.DefineMacro":
+            {
+                string mname = pt.children[0].matches[0];
+                string[] argnames = pt.children[1].children.map!(x => x.matches[0]).array;
+                string code = pt.children[2].matches[0].strip;
+                // TODO:Support manual overrides
+                dmod.code ~= "auto ";
+                dmod.code ~= mname;
+                dmod.code ~= "(";
+                foreach(i, a; argnames) // template type arguments
+                {
+                    dmod.code ~= a;
+                    dmod.code ~= "_T__, ";
+                }
+                dmod.code ~= ")(";
+                foreach(i, a; argnames) // real arguments
+                {
+                    dmod.code ~= a;
+                    dmod.code ~= "_T__ ";
+                    dmod.code ~= a;
+                    dmod.code ~= ", ";
+                }
+                dmod.code ~= ")\n{\n    return ";
+                dmod.code ~= code;
+                dmod.code ~= ";\n}\n\n";
+                break;
+            }
+        default:
+            //throw new Exception("Unexpected parsetree node: " ~ pt.name);
+            break;
+        }
+    }
+
+    parse(p);
+    static import std.file;
+
+    std.file.write("testout.d", dmod.code.data);
 }
 
 ParseTree warnUnrecognisedDecl(ParseTree pt)
 {
     string match = pt.input[pt.begin .. pt.end];
-    if(match.length < 1)
+    if (match.length < 1)
         return pt;
     stderr.writefln("Unrecognised code: %s", match);
     return pt;
+}
+
+ParseTree dropToChild(ParseTree pt)
+{
+    if (pt.successful && pt.children.length > 0)
+    {
+        return pt.children[0];
+    }
+    else
+    {
+        return pt;
+    }
+}
+
+ParseTree* getChild(ParseTree* pt, string name)
+{
+    foreach (ref c; pt.children)
+        if (c.name == name)
+            return &c;
+    return null;
 }
 
 mixin(grammar(PEG_C_GRAMMAR));
 enum string PEG_C_GRAMMAR = q{
 CCode:
 
-    Unit <- (Spacing? (
-            / PreprocessorOnlyLine
+    Unit <- (Spacing {dropToChild} (
+            / PreprocessorOnlyLine {dropToChild}
             / DefineMacro
             / DefineConstant
             / AliasTypedef
-            / LoneDefinition
+            / LoneDefinition {dropToChild}
             / UnrecognisedDecl
             ))+
 
@@ -159,9 +325,10 @@ CCode:
     PElif <- "#elif" UntilEOL
     PElse <- "#else" endOfLine
     PEndIf <- "#endif" endOfLine
-    PError <- "#error" UntilEOL
+    PError <- "#error" Spacing UntilEOL
 
-    Spacing <~ (:space / :endOfLine / DocComment / :Comment)*
+    ForceSpacing <- (DocComment / :space / :Comment)+
+    Spacing <- (DocComment / :space / :endOfLine / :Comment)*
     Identifier <~ [a-zA-Z_] [a-zA-Z0-9_]*
     Number <~ [0-9]+
     ArrayPostfix < "[" Number? "]"
@@ -169,23 +336,20 @@ CCode:
     UntilEOL <~ (!endOfLine .)* :endOfLine
 
     LoneDefinition < StructType / UnionType ";"
-    StructType <- "struct" Spacing? Identifier? Spacing? "{" Spacing? FieldDecl* Spacing? "}"
-    UnionType <- "union" Spacing? Identifier? Spacing? "{" Spacing? FieldDecl* Spacing? "}"
-    Pointer <- Type Spacing? "*"
+    StructType <- "struct" Spacing Identifier? Spacing "{" Spacing FieldDecl* Spacing "}"
+    UnionType <- "union" Spacing Identifier? Spacing "{" Spacing FieldDecl* Spacing "}"
+    Pointer <- Type Spacing "*"
     Type <- StructType / UnionType / Pointer / Identifier
     Argument < "IN"? "OUT"? Type Identifier? ArrayPostfix? "OPTIONAL"?
     FuncPointerVar < Type "(" "EFIAPI" "*" Identifier? ")" "(" (Argument ",")* Argument? ")"
 
-    GuardStart <- "#ifndef" Spacing Identifier endOfLine "#define" Spacing Identifier endOfLine
-    GuardEnd <- "#endif"
-    GuardedBlock <- GuardStart Unit GuardEnd
+    Path <~ [-a-zA-Z_./]+
+    Include <- "#include" Spacing ("<"/doublequote) Path (">"/doublequote) endOfLine
+    MacroArguments < (Identifier ",")* Identifier?
+    DefineMacro <- "#define" Spacing Identifier "(" MacroArguments ")" UntilEOL
+    DefineConstant <- "#define" Spacing Identifier (endOfLine / UntilEOL)
 
-    Path <- [a-zA-Z_./]+
-    Include < "#include" ("<"/doublequote) Path (">"/doublequote) endOfLine
-    DefineMacro <- "#define" Spacing Identifier ~("(" (!")" .)* ")") UntilEOL
-    DefineConstant <- "#define" Spacing Identifier Spacing UntilEOL
-
-    AliasTypedef <- "typedef" Spacing (FuncPointerVar / Type Spacing Identifier Spacing? ArrayPostfix?) Spacing? ";"
+    AliasTypedef < "typedef" (FuncPointerVar / Type Identifier ArrayPostfix?) ";"
     FieldsBlock < FieldDecl*
     FieldDecl < (FuncPointerVar / Type Identifier? (ArrayPostfix / BitsPostfix)?) ";"
     
---
+++ tools/testout.d
@@ -0,0 +1,239 @@
+
+//=====================================
+// Uefi/UefiBaseType.h
+//=====================================
+/** @file
+  Defines data types and constants introduced in UEFI.
+
+Copyright (c) 2006 - 2017, Intel Corporation. All rights reserved.<BR>
+Portions copyright (c) 2011 - 2016, ARM Ltd. All rights reserved.<BR>
+
+This program and the accompanying materials are licensed and made available under 
+the terms and conditions of the BSD License that accompanies this distribution.  
+The full text of the license may be found at
+http://opensource.org/licenses/bsd-license.php.                                          
+    
+THE PROGRAM IS DISTRIBUTED UNDER THE BSD LICENSE ON AN "AS IS" BASIS,                     
+WITHOUT WARRANTIES OR REPRESENTATIONS OF ANY KIND, EITHER EXPRESS OR IMPLIED.
+
+**/
+//HDRERROR: Missing mapping to Base.h
+///
+/// 128-bit buffer containing a unique identifier value.
+///
+
+alias EFI_GUID = GUID;
+alias EFI_STATUS = RETURN_STATUS;
+alias EFI_HANDLE = VOID*;
+alias EFI_EVENT = VOID*;
+alias EFI_TPL = UINTN;
+alias EFI_LBA = UINT64;
+alias EFI_PHYSICAL_ADDRESS = UINT64;
+alias EFI_VIRTUAL_ADDRESS = UINT64;
+alias EFI_IPv4_ADDRESS = IPv4_ADDRESS;
+alias EFI_IPv6_ADDRESS = IPv6_ADDRESS;
+enum EFI_SUCCESS =                RETURN_SUCCESS              ;
+enum EFI_LOAD_ERROR =             RETURN_LOAD_ERROR           ;
+enum EFI_INVALID_PARAMETER =      RETURN_INVALID_PARAMETER    ;
+enum EFI_UNSUPPORTED =            RETURN_UNSUPPORTED          ;
+enum EFI_BAD_BUFFER_SIZE =        RETURN_BAD_BUFFER_SIZE      ;
+enum EFI_BUFFER_TOO_SMALL =       RETURN_BUFFER_TOO_SMALL     ;
+enum EFI_NOT_READY =              RETURN_NOT_READY            ;
+enum EFI_DEVICE_ERROR =           RETURN_DEVICE_ERROR         ;
+enum EFI_WRITE_PROTECTED =        RETURN_WRITE_PROTECTED      ;
+enum EFI_OUT_OF_RESOURCES =       RETURN_OUT_OF_RESOURCES     ;
+enum EFI_VOLUME_CORRUPTED =       RETURN_VOLUME_CORRUPTED     ;
+enum EFI_VOLUME_FULL =            RETURN_VOLUME_FULL          ;
+enum EFI_NO_MEDIA =               RETURN_NO_MEDIA             ;
+enum EFI_MEDIA_CHANGED =          RETURN_MEDIA_CHANGED        ;
+enum EFI_NOT_FOUND =              RETURN_NOT_FOUND            ;
+enum EFI_ACCESS_DENIED =          RETURN_ACCESS_DENIED        ;
+enum EFI_NO_RESPONSE =            RETURN_NO_RESPONSE          ;
+enum EFI_NO_MAPPING =             RETURN_NO_MAPPING           ;
+enum EFI_TIMEOUT =                RETURN_TIMEOUT              ;
+enum EFI_NOT_STARTED =            RETURN_NOT_STARTED          ;
+enum EFI_ALREADY_STARTED =        RETURN_ALREADY_STARTED      ;
+enum EFI_ABORTED =                RETURN_ABORTED              ;
+enum EFI_ICMP_ERROR =             RETURN_ICMP_ERROR           ;
+enum EFI_TFTP_ERROR =             RETURN_TFTP_ERROR           ;
+enum EFI_PROTOCOL_ERROR =         RETURN_PROTOCOL_ERROR       ;
+enum EFI_INCOMPATIBLE_VERSION =   RETURN_INCOMPATIBLE_VERSION ;
+enum EFI_SECURITY_VIOLATION =     RETURN_SECURITY_VIOLATION   ;
+enum EFI_CRC_ERROR =              RETURN_CRC_ERROR   ;
+enum EFI_END_OF_MEDIA =           RETURN_END_OF_MEDIA;
+enum EFI_END_OF_FILE =            RETURN_END_OF_FILE;
+enum EFI_INVALID_LANGUAGE =       RETURN_INVALID_LANGUAGE;
+enum EFI_COMPROMISED_DATA =       RETURN_COMPROMISED_DATA;
+enum EFI_HTTP_ERROR =             RETURN_HTTP_ERROR;
+enum EFI_WARN_UNKNOWN_GLYPH =     RETURN_WARN_UNKNOWN_GLYPH   ;
+enum EFI_WARN_DELETE_FAILURE =    RETURN_WARN_DELETE_FAILURE  ;
+enum EFI_WARN_WRITE_FAILURE =     RETURN_WARN_WRITE_FAILURE   ;
+enum EFI_WARN_BUFFER_TOO_SMALL =  RETURN_WARN_BUFFER_TOO_SMALL;
+enum EFI_WARN_STALE_DATA =        RETURN_WARN_STALE_DATA;
+enum EFI_WARN_FILE_SYSTEM =       RETURN_WARN_FILE_SYSTEM;
+///@}
+
+///
+/// Define macro to encode the status code.
+/// 
+
+auto EFIERR(_a_T__, )(_a_T__ _a, )
+{
+    return ENCODE_ERROR(_a);
+}
+
+auto EFI_ERROR(A_T__, )(A_T__ A, )
+{
+    return RETURN_ERROR(A);
+}
+
+///
+/// ICMP error definitions
+///@{
+
+enum EFI_NETWORK_UNREACHABLE =    EFIERR(100);
+enum EFI_HOST_UNREACHABLE =       EFIERR(101) ;
+enum EFI_PROTOCOL_UNREACHABLE =   EFIERR(102);
+enum EFI_PORT_UNREACHABLE =       EFIERR(103);
+///@}
+
+///
+/// Tcp connection status definitions
+///@{
+
+enum EFI_CONNECTION_FIN =         EFIERR(104);
+enum EFI_CONNECTION_RESET =       EFIERR(105);
+enum EFI_CONNECTION_REFUSED =     EFIERR(106);
+///@}
+
+enum EFI_PAGE_SIZE =              SIZE_4KB;
+enum EFI_PAGE_MASK =              0xFFF;
+enum EFI_PAGE_SHIFT =             12;
+/**
+  Macro that converts a size, in bytes, to a number of EFI_PAGESs.
+
+  @param  Size      A size in bytes.  This parameter is assumed to be type UINTN.  
+                    Passing in a parameter that is larger than UINTN may produce 
+                    unexpected results.
+
+  @return  The number of EFI_PAGESs associated with the number of bytes specified
+           by Size.
+
+**/
+auto EFI_SIZE_TO_PAGES(Size_T__, )(Size_T__ Size, )
+{
+    return (((Size) >> EFI_PAGE_SHIFT) + (((Size) & EFI_PAGE_MASK) ? 1 : 0));
+}
+
+/**
+  Macro that converts a number of EFI_PAGEs to a size in bytes.
+
+  @param  Pages     The number of EFI_PAGES.  This parameter is assumed to be 
+                    type UINTN.  Passing in a parameter that is larger than 
+                    UINTN may produce unexpected results.
+
+  @return  The number of bytes associated with the number of EFI_PAGEs specified 
+           by Pages.
+  
+**/
+auto EFI_PAGES_TO_SIZE(Pages_T__, )(Pages_T__ Pages, )
+{
+    return ((Pages) << EFI_PAGE_SHIFT);
+}
+
+///
+/// PE32+ Machine type for IA32 UEFI images.
+///
+
+enum EFI_IMAGE_MACHINE_IA32 =             0x014C;
+///
+/// PE32+ Machine type for IA64 UEFI images.
+///
+
+enum EFI_IMAGE_MACHINE_IA64 =             0x0200;
+///
+/// PE32+ Machine type for EBC UEFI images.
+///
+
+enum EFI_IMAGE_MACHINE_EBC =              0x0EBC;
+///
+/// PE32+ Machine type for X64 UEFI images.
+///
+
+enum EFI_IMAGE_MACHINE_X64 =              0x8664;
+///
+/// PE32+ Machine type for ARM mixed ARM and Thumb/Thumb2 images.
+///
+
+enum EFI_IMAGE_MACHINE_ARMTHUMB_MIXED =   0x01C2;
+///
+/// PE32+ Machine type for AARCH64 A64 images.
+///
+
+enum EFI_IMAGE_MACHINE_AARCH64 =   0xAA64;
+auto EFI_IMAGE_MACHINE_TYPE_SUPPORTED(Machine_T__, )(Machine_T__ Machine, )
+{
+    return (((Machine) == EFI_IMAGE_MACHINE_IA32) || ((Machine) == EFI_IMAGE_MACHINE_EBC));
+}
+
+auto EFI_IMAGE_MACHINE_CROSS_TYPE_SUPPORTED(Machine_T__, )(Machine_T__ Machine, )
+{
+    return ((Machine) == EFI_IMAGE_MACHINE_X64);
+}
+
+auto EFI_IMAGE_MACHINE_TYPE_SUPPORTED(Machine_T__, )(Machine_T__ Machine, )
+{
+    return (((Machine) == EFI_IMAGE_MACHINE_IA64) || ((Machine) == EFI_IMAGE_MACHINE_EBC));
+}
+
+auto EFI_IMAGE_MACHINE_CROSS_TYPE_SUPPORTED(Machine_T__, )(Machine_T__ Machine, )
+{
+    return (FALSE);
+}
+
+auto EFI_IMAGE_MACHINE_TYPE_SUPPORTED(Machine_T__, )(Machine_T__ Machine, )
+{
+    return (((Machine) == EFI_IMAGE_MACHINE_X64) || ((Machine) == EFI_IMAGE_MACHINE_EBC));
+}
+
+auto EFI_IMAGE_MACHINE_CROSS_TYPE_SUPPORTED(Machine_T__, )(Machine_T__ Machine, )
+{
+    return ((Machine) == EFI_IMAGE_MACHINE_IA32);
+}
+
+auto EFI_IMAGE_MACHINE_TYPE_SUPPORTED(Machine_T__, )(Machine_T__ Machine, )
+{
+    return ((Machine) == EFI_IMAGE_MACHINE_ARMTHUMB_MIXED);
+}
+
+auto EFI_IMAGE_MACHINE_CROSS_TYPE_SUPPORTED(Machine_T__, )(Machine_T__ Machine, )
+{
+    return (FALSE);
+}
+
+auto EFI_IMAGE_MACHINE_TYPE_SUPPORTED(Machine_T__, )(Machine_T__ Machine, )
+{
+    return (((Machine) == EFI_IMAGE_MACHINE_AARCH64) || ((Machine) == EFI_IMAGE_MACHINE_EBC));
+}
+
+auto EFI_IMAGE_MACHINE_CROSS_TYPE_SUPPORTED(Machine_T__, )(Machine_T__ Machine, )
+{
+    return (FALSE);
+}
+
+///
+/// This is just to make sure you can cross compile with the EBC compiler.
+/// It does not make sense to have a PE loader coded in EBC. 
+///
+
+auto EFI_IMAGE_MACHINE_TYPE_SUPPORTED(Machine_T__, )(Machine_T__ Machine, )
+{
+    return ((Machine) == EFI_IMAGE_MACHINE_EBC);
+}
+
+auto EFI_IMAGE_MACHINE_CROSS_TYPE_SUPPORTED(Machine_T__, )(Machine_T__ Machine, )
+{
+    return (FALSE);
+}
+
+static assert(0, `Unknown Processor Type`);
